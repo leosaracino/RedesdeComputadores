ac8956176a1fb2e6287c291b07167a57ead154e3
leo saracino <leosaracino2211@gmail.com>
2022-09-20 17:18:45 -0300

    first commit

---
+++ network.py
@@ -0,0 +1,42 @@
+import socket
+from _thread import *
+
+class Network():
+
+    def reciever(self):
+        while True:
+            data = self.client.recv(4096)
+            self.last_response = data.decode()
+            if(self.last_response.startswith("play")):
+                if self.last_response.split("/")[1] == "ready":
+                    self.ready = True
+                else:
+                    self.ready = False
+
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "192.168.1.105"
+        self.port = 5555
+        self.addr = (self.server,self.port)
+        self.ready = False
+        self.id = self.connect()
+        self.last_response = ""
+        start_new_thread(self.reciever, ())
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+
+
+    def send(self, msg):
+        # print("enviou: " + msg.decode())
+        try:
+            self.client.send(msg)
+        except socket.error as e:
+            print(e)
+
+    def close(self):
+        self.client.close()
